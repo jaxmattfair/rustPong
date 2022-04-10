a41189f500158e7df62ff7a95787781ab608eb1e
Jackson <jaxsofair@gmail.com>
2022-04-10 00:13:56 -0400

    Deleted target directory
    Auto-gen file by cargo run

--- target/debug/deps/pong-afafda1270f165b8.d
+++
@@ -1,5 +0,0 @@
-/Users/jax/Desktop/Rust/rustPong/target/debug/deps/pong-afafda1270f165b8: src/main.rs
-
-/Users/jax/Desktop/Rust/rustPong/target/debug/deps/pong-afafda1270f165b8.d: src/main.rs
-
-src/main.rs:
